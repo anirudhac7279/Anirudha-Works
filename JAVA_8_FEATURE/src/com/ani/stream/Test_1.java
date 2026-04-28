bebf71dc6a5a2949119e702f0c7b2cf440b85a64
anirudhac7279 <anirudhac7279@gmail.com>
2026-04-28 22:23:59 +0530

    ANIRUDHA

---
+++ JAVA_8_FEATURE/src/com/ani/stream/Test_1.java
@@ -0,0 +1,7 @@
+package com.ani.stream;
+
+public class Test_1 {
+public static void main(String[] args) {
+	System.out.println("ANIR");
+}
+}
--- JAVA_8_PRACTISE/src/com/ani/intermediate/Test_1.java
+++
@@ -1,5 +0,0 @@
-package com.ani.intermediate;
-
-public class Test_1 {
-
-}
